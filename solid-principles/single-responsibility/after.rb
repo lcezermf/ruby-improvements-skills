006d076bbfb6752d069813a87ab570115d19efd2
Luiz Cezer <lccezinha@gmail.com>
2014-11-11 22:58:02 -0200

    Exemplo de SRP

--- solid-principles/single-responsibility/after.rb
+++ solid-principles/single-responsibility/after.rb
@@ -1,1 +1,18 @@
-#WIP
+=begin
+Classe Product possui a lógica de calculo do valor com desconto,
+caso seja necessário mudar o valor de desconto de 10% para 20% ou mudar a regra será preciso alterar essa classe,
+mas Product não deve saber dessa lógica e sim apenas guardar dados de Product
+=end
+class Product
+  attr_reader :price
+
+  def initialize(name, price)
+    @name  = name
+    @price = price
+  end
+
+  # necessário uma classe p/ realizar encapsular a lógica de cálculo de desconto
+  def price_with_discount
+    @price * 0.90
+  end
+end
--- solid-principles/single-responsibility/before.rb
+++ solid-principles/single-responsibility/before.rb
@@ -1,1 +1,27 @@
-#WIP
+=begin
+Agora a classe Product não realiza mais o cálculo de desconto e sim delega esse cálculo p/ a classe responsável.
+=end
+class Product
+  attr_reader :price
+
+  def initialize(name, price)
+    @name  = name
+    @price = price
+  end
+
+  def price_with_discount
+    DiscountCalculator.new(price).calculate
+  end
+end
+
+class DiscountCalculator
+  DiscountPercentage = 0.90
+
+  def initialize(value)
+    @value = value
+  end
+
+  def calculate
+    @value * DiscountPercentage
+  end
+end
