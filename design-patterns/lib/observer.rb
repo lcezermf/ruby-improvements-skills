0372ad8e89d6b09503833e3b08dc2c38ab302ad2
Luiz Cezer <lccezinha@gmail.com>
2015-03-10 19:19:23 -0300

    Ativando curse de um jeito meio :shit:

--- design-patterns/lib/observer.rb
+++ design-patterns/lib/observer.rb
@@ -1,12 +1,19 @@
 # Behavior Pattern
 
 class Hero
+  attr_reader :health
+
   def initialize
     @cursed = false
+    @health = 10
   end
 
   def cursed?
     @cursed
+  end
+
+  def damage(hit)
+    @health -= hit
   end
 
   def discover(tile)
@@ -17,9 +24,14 @@ end
 class Tile
   def initialize(attrs = {})
     @cursed = attrs.fetch(:cursed, false)
+    @hero = attrs.fetch(:hero, nil)
   end
 
   def cursed?
     @cursed
+  end
+
+  def activate_curse
+    @hero.damage(4)
   end
 end
--- design-patterns/spec/observer_spec.rb
+++ design-patterns/spec/observer_spec.rb
@@ -4,8 +4,9 @@ require_relative '../lib/observer'
 describe 'Observer Pattern' do
 
   describe Hero do
+    let(:hero) { Hero.new }
+
     it 'its cursed then discovers cursed Tile' do
-      hero = Hero.new
       tile = Tile.new cursed: true
       hero.discover(tile)
 
@@ -13,15 +14,31 @@ describe 'Observer Pattern' do
     end
 
     it 'its not cursed when discovers title without curse' do
-      hero = Hero.new
       tile = Tile.new
       hero.discover(tile)
 
       expect(hero.cursed?).to be_falsy
     end
+
+    it 'it has default health eq to 10' do
+      expect(hero.health).to eq(10)
+    end
+
+    it 'can be damaged' do
+      hero.damage(6)
+      expect(hero.health).to eq(4)
+    end
   end
 
   describe Tile do
+    it 'activetes curse' do
+      hero = Hero.new
+      tile = Tile.new cursed: true, hero: hero
+
+      tile.activate_curse
+      expect(hero.health).to eq(6)
+    end
+
     it 'its not cursed by default' do
       tile = Tile.new
 
