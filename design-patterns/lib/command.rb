77e8bfcfc0482b6864efdcd33c3fd76212444e20
Luiz Cezer <lccezinha@gmail.com>
2015-03-31 22:00:13 -0300

    Undo

--- design-patterns/lib/command.rb
+++ design-patterns/lib/command.rb
@@ -1,3 +1,81 @@
+class Turn
+  attr_accessor :moves
+
+  def initialize
+    @moves = []
+  end
+
+  def make_move(move)
+    move.execute
+    @moves << move
+  end
+
+  def undo_move
+    @moves.pop.unexecute
+  end
+
+end
+
+class Hero
+  attr_accessor :wood, :health, :money
+
+  def initialize
+    @wood = 0
+    @health = 0
+    @money = 0
+  end
+end
+
+class ChopWoodCommand
+  attr_accessor :hero
+
+  def initialize(hero)
+    @hero = hero
+  end
+
+  def execute
+    hero.wood += 10
+  end
+
+  def unexecute
+    hero.wood -= 10
+  end
+end
+
+class HealCharacterCommand
+  attr_accessor :hero
+
+  def initialize(hero)
+    @hero = hero
+  end
+
+  def execute
+    hero.health += 5
+  end
+
+  def unexecute
+    hero.health -= 5
+  end
+end
+
+class GetMoneyCommand
+  attr_accessor :hero
+
+  def initialize(hero)
+    @hero = hero
+  end
+
+  def execute
+    hero.money += 50
+  end
+
+  def unexecute
+    hero.money -= 50
+  end
+end
+
+####
+
 class Reactor
 
   def initialize
