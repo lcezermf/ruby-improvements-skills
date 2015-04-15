7039b6a21b1590ca60dc2f2b40aa3ae456b06e86
Luiz Cezer <lccezinha@gmail.com>
2015-04-14 22:25:21 -0300

    Singleton usando módulo do Ruby

--- design-patterns/lib/singleton_pattern.rb
+++ design-patterns/lib/singleton_pattern.rb
@@ -1,13 +1,10 @@
+require 'singleton'
+
 class HeavyMetal; end
 class ThrashMetal; end
 
 class FestivalFactory
-
-  @@instance = nil
-
-  def self.instance
-    @@instance ||= FestivalFactory.send(:new)
-  end
+  include Singleton
 
   def create_heavy_metal
     HeavyMetal.new
@@ -15,6 +12,4 @@ class FestivalFactory
   def create_thrash_metal
     ThrashMetal.new
   end
-
-  private_class_method :new
 end
