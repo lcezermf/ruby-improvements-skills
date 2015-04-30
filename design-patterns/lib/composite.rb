b01cc0c6ce3d26a9cdd6813312d680fbac9291b9
Luiz Cezer <lccezinha@gmail.com>
2015-04-30 20:19:08 -0300

    Quest composta de Tasks

--- design-patterns/lib/composite.rb
+++ design-patterns/lib/composite.rb
@@ -1,3 +1,21 @@
+class Quest
+  def initialize
+    @tasks = []
+  end
+
+  def add(task)
+    tasks << task
+  end
+
+  def reward
+    tasks.inject(0) { |sum, task| sum += task.reward }
+  end
+
+  private
+
+  attr_reader :tasks
+end
+
 class Encounter
   attr_reader :reward
 
