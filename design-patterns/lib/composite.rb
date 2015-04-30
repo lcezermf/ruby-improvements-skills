a8c5bed335f47f944edcce07adc8b54ba9ee7a3c
Luiz Cezer <lccezinha@gmail.com>
2015-04-30 20:28:51 -0300

    MegaQuest feita de Quest + Task

--- design-patterns/lib/composite.rb
+++ design-patterns/lib/composite.rb
@@ -1,3 +1,22 @@
+# Structural Pattern
+class MegaQuest
+  def initialize
+    @tasks = []
+  end
+
+  def add(task)
+    tasks << task
+  end
+
+  def reward
+    tasks.inject(0) { |sum, task| sum += task.reward }
+  end
+
+  private
+
+  attr_reader :tasks
+end
+
 class Quest
   def initialize
     @tasks = []
--- design-patterns/spec/composite_spec.rb
+++ design-patterns/spec/composite_spec.rb
@@ -3,6 +3,25 @@ require_relative '../lib/composite'
 
 describe 'Composite Pattern' do
 
+  describe MegaQuest do
+    it 'consists from 2 quest + 1 Encounter' do
+      quest_one = Quest.new
+      quest_one.add(Puzzle.new)
+      quest_one.add(Encounter.new)
+
+      quest_two = Quest.new
+      quest_two.add(Puzzle.new)
+      quest_two.add(Diplomacy.new)
+
+      mega = MegaQuest.new
+      mega.add(quest_one)
+      mega.add(quest_two)
+      mega.add(Puzzle.new)
+
+      expect(mega.reward).to eq(1200)
+    end
+  end
+
   describe Quest do
     context 'with Encounter and Diplomacy' do
       it 'has reward of 600' do
