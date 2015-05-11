5720008a3bf72e13c0fba4541c23ed5f8cc212f3
Luiz Cezer <lccezinha@gmail.com>
2015-05-11 14:28:11 -0300

    Limpando Item

--- design-patterns/lib/decorator.rb
+++ design-patterns/lib/decorator.rb
@@ -33,18 +33,9 @@ end
 class Item
   attr_reader :price, :description
 
-  def initialize(is_magic = false, is_masterpiece = false)
+  def initialize
     @price = 10
     @description  = 'Item.'
-
-    if is_magic
-      @price *= 3
-      @description += 'Magic.'
-    end
-    if is_masterpiece
-      @price *= 2
-      @description += 'Masterpiece.'
-    end
   end
 
   def use; end
