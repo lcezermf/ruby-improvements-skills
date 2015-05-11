94f67ec824b91319bae7b1b2af424007fe39a5e9
Luiz Cezer <lccezinha@gmail.com>
2015-05-11 09:47:55 -0300

    Add ItemDecorator p/ decorator base

--- design-patterns/lib/decorator.rb
+++ design-patterns/lib/decorator.rb
@@ -1,14 +1,6 @@
-class MagicItemDecorator
+class ItemDecorator
   def initialize(item)
     @item = item
-  end
-
-  def price
-    @item.price * 3
-  end
-
-  def description
-    "#{@item.description}Magic."
   end
 
   def use
@@ -16,21 +8,23 @@ class MagicItemDecorator
   end
 end
 
-class MasterpieceItemDecorator
-  def initialize(item)
-    @item = item
+class MagicItemDecorator < ItemDecorator
+  def price
+    @item.price * 3
   end
 
+  def description
+    "#{@item.description}Magic."
+  end
+end
+
+class MasterpieceItemDecorator < ItemDecorator
   def price
     @item.price * 2
   end
 
   def description
     "#{@item.description}Masterpiece."
-  end
-
-  def use
-    @item.use
   end
 end
 
