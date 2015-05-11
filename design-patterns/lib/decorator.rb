e5214e8148b18f5a53d81b715717da4f87de415f
Luiz Cezer <lccezinha@gmail.com>
2015-05-11 09:34:58 -0300

    Add MagicItem

--- design-patterns/lib/decorator.rb
+++ design-patterns/lib/decorator.rb
@@ -1,9 +1,14 @@
 class Item
   attr_reader :price, :description
 
-  def initialize
+  def initialize(is_magic = false)
     @price = 10
     @description  = 'Item.'
+
+    if is_magic
+      @price *= 3
+      @description += 'Magic.'
+    end
   end
 
   def use; end
--- design-patterns/spec/decorator_spec.rb
+++ design-patterns/spec/decorator_spec.rb
@@ -4,6 +4,18 @@ require_relative '../lib/decorator'
 describe 'Decorator Pattern' do
   let(:common_item) { Item.new }
 
+  context 'magic item' do
+    let(:magic_item) { Item.new true }
+
+    it 'has price 3 times more expansive' do
+      expect(magic_item.price).to eq(common_item.price * 3)
+    end
+
+    it 'has description' do
+      expect(magic_item.description).to eq("#{common_item.description}Magic.")
+    end
+  end
+
   context 'common_item' do
     it 'has prince' do
       expect(common_item.price).to eq(10)
